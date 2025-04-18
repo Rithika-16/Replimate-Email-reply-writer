51d68dffda00e3fbaa97ccf414d23321e2700293
Rithika 16 <rithikakaruppusamy143@gmail.com>
2025-04-19 00:42:05 +0530

    Initial commit

---
+++ email-writer-sb/src/test/java/com/email/email/writer/EmailWriterSbApplicationTests.java
@@ -0,0 +1,14 @@
+package com.email.email.writer;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWriterSbApplicationTests {
+
+	@Test
+	@Disabled // Add this
+	void contextLoads() {
+	}
+}
